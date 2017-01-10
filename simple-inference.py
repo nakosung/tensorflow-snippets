03d80301b462abcee5d5d61096640635f3837e14
Nako Sung <deif79@gmail.com>
2017-01-11 00:36:01 +0900

    ...

--- simple-inference.py
+++ simple-inference.py
@@ -8,7 +8,7 @@ W = tf.get_variable(name='W',shape=(1,1))
 b = tf.get_variable(name='b',shape=(1,1))
 O = tf.add(tf.matmul(X,W),b,name='Out')
 dist = O - Y
-loss = tf.reduce_sum(dist * dist) 
+loss = tf.reduce_sum(dist * dist)
 opt = tf.train.AdamOptimizer()
 train = opt.minimize(loss)
 
@@ -20,22 +20,22 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess:
     os.system("rm -rf ./graph")
     tf.train.write_graph(sess.graph_def,'./graph','graph.pb',as_text=False)
-        
+
     sess.run(init)
-    
+
     for i in range(1000):
         _, l = sess.run([train,loss],feed_dict={X:x_batch,Y:y_batch})
         print(i,l)
 
     # save trainable var's weight
-    saver = tf.train.Saver()    
+    saver = tf.train.Saver()
     saver.save(sess,'./graph/test')
-    
+
     # trainable var + graph_def ==> frozen graph
     freeze_graph(
         './graph/graph.pb',
         '',
-        True,        
+        True,
         './graph/test',
         'Out',
         'save/restore_all',
@@ -55,11 +55,11 @@ X = tf.placeholder(tf.float32,[None,1])
 with gfile.FastGFile('./graph/frozen.pb', 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
-    
+
     tf.import_graph_def(graph_def,input_map={'X':X})
 
 graph = tf.get_default_graph()
 Out = graph.get_tensor_by_name("import/Out:0")
-with tf.Session() as sess:        
+with tf.Session() as sess:
     out = sess.run(Out,feed_dict={X:[[1.],[2.]]})
     print(out)
